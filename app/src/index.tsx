9bab7f195add35dc74f9a03d7d8c387180b6d30a
knightlore <gnightshade@gmail.com>
2019-03-08 19:13:28 -0300

    init

---
+++ app/src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Comp } from "../../lib/component/comp";
+
+function App() {
+
+    return <div>
+        <Comp />
+    </div>
+
+}
---
+++ lib/component/comp.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export function Comp() {
+    return <div>Component</div>
+}
